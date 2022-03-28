488dc19028d2679f2ed86d1e352ff9b45a2dadc3
Aum Pauskar <aumpauskar111@gmail.com>
2022-03-28 10:08:58 +0530

    Migrations edited

--- cal/admin.py
+++ cal/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from . models import Cal
+class CalAdmin(admin.CalAdmin):
+	list_display = ('sr', 'event', 'date', 'notes', 
+	'Img_url')
--- cal/models.py
+++ cal/models.py
@@ -2,9 +2,9 @@ from django.db import models
 # Create your models here.
 
 class Cal(models.Model):
-    Sr = models.IntegerField()
-    Event = models.CharField(max_length=1024)
-    Date = models.DateField()
-    Note = models.CharField(max_length=256)
-    Img_url = models.CharField(max_length=2048)
+    sr = models.IntegerField()
+    event = models.CharField(max_length=1024)
+    date = models.DateField()
+    note = models.CharField(max_length=256)
+    img_url = models.CharField(max_length=2048)
 
