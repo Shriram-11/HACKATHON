572b6dd828be0f30f6c0906fdd064177916dd3c3
Aum Pauskar <aumpauskar111@gmail.com>
2022-03-28 09:16:07 +0530

    Hello world done - models pending

--- cal/models.py
+++ cal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
 # Create your models here.
+
+
+'''
+from django.db import models
+# Create your models here.
+class Product(models.Model):
+names = models.CharField(max_length=255)
+price = models.FloatField()
+stock = models.IntegerField()
+image_url = models.CharField(max_length=2047)
+'''
+class Cal(models.Model):
