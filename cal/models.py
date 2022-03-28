3e498af0c577542a66428f755509a96239863fe6
SHRIRAM <nshriram1326@gmail.com>
2022-03-28 09:30:52 +0530

    Fileds Added- Model

--- cal/models.py
+++ cal/models.py
@@ -1,14 +1,10 @@
 from django.db import models
 # Create your models here.
 
-
-'''
-from django.db import models
-# Create your models here.
-class Product(models.Model):
-names = models.CharField(max_length=255)
-price = models.FloatField()
-stock = models.IntegerField()
-image_url = models.CharField(max_length=2047)
-'''
 class Cal(models.Model):
+    Sr = models.IntegerField()
+    Event = models.CharField(max_length=1024)
+    Date = models.DateField()
+    Note = models.CharField(max_length=256)
+    Img_url = models.CharField(max_length=2048)
+
